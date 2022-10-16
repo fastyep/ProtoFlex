b30a4ba96f87097102b4d1445bb6dce815b395b8
fastyep <fast1998991@gmail.com>
2022-10-16 22:03:31 +0500

    init

---
+++ ProtoFlex/Program.fs
@@ -0,0 +1,48 @@
+﻿module ProtoFlex.CLI
+
+open System.IO
+open Argu
+open Argu.ArguAttributes
+open ProtoFlex.Generator
+
+[<RequireQualifiedAccess>]
+type CliArguments =
+    | [<AltCommandLine "-i">] InputFile of path: string
+    | [<AltCommandLine "-o"; Unique>] OutputFile of path: string
+    | [<AltCommandLine "-n"; Unique>] Namespace of name: string
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | InputFile _ -> "path to proto files to build."
+            | OutputFile _ -> "path where to write final FSharp types bundle."
+            | Namespace _ -> "namespace that will be used in final bundle file."
+
+[<EntryPoint>]
+let main args =
+    let parser =
+        ArgumentParser.Create<CliArguments>(programName = "flex")
+
+    try
+        let result = parser.ParseCommandLine args
+
+        let input =
+            result.GetResults CliArguments.InputFile
+            |> List.map (fun a -> Path.GetFileName a, File.OpenText a |> unbox)
+
+        let name_space =
+            result.TryGetResult CliArguments.Namespace
+
+        match result.TryGetResult CliArguments.OutputFile with
+        | Some output when (input.Length > 0) ->
+            match genTemplate name_space input with
+            | Ok temp ->
+                File.WriteAllText(output, temp)
+                printfn $"File {Path.GetFileName output} has been successfully generated."
+            | Error err ->
+                printfn "Errors during parsing proto file: "
+                err |> Seq.iter (printfn "\t%A")
+        | _ -> printfn "Output and at least one Input files should be specified!"
+    with
+    | :? ArguParseException as e -> printfn $"%s{e.Message}"
+
+    0
